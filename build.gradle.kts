3400aa9fe87efdaef092c61b550877fb3c0c0259
Nikita <itname@bk.ru>
2026-03-24 20:15:31 +0700

    Добавлен MVStore и jackson для работы с JSON

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.4")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.4")
 }
 
 tasks.test {
