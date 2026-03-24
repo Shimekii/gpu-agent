ea876eb4f566bfd34aa1fcafa51a2846d395eb76
Nikita <itname@bk.ru>
2026-03-24 20:41:34 +0700

    Исправлена команда show и добавлено описание в Readme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 import org.example.service.GPUAgent;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final GPUAgent agent = new GPUAgent();
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        GPUAgent agent = new GPUAgent();
         boolean running = true;
         while(running){
             String line = scanner.nextLine().trim();
@@ -20,12 +23,21 @@ public class Main {
                     agent.stopMonitoring();
                     break;
                 case "show":
-                    agent.showData("owner");
+                    commandShow(Arrays.copyOfRange(commandArgs, 1, commandArgs.length));
                     break;
                 case "exit":
                     agent.close();
                     System.exit(0);
             }
+        }
+    }
+
+    private static void commandShow(String[] args){
+        if (args.length == 0) {
+            System.out.println("Username is empty");
+        }
+        else {
+            agent.showData(args[0]);
         }
     }
 }
