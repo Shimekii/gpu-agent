4b390dd611dd6a98efce384c2a6dc61055749945
Nikita <itname@bk.ru>
2026-03-24 20:17:50 +0700

    Сервис для работы с MVStore

---
+++ src/main/java/org/example/service/StorageService.java
@@ -0,0 +1,30 @@
+package org.example.service;
+
+import org.h2.mvstore.MVStore;
+
+import java.util.Map;
+
+public class StorageService {
+    private final MVStore store;
+    private final Map<String, Object> map;
+
+    public StorageService(String fileName, String mapName){
+        store = new MVStore.Builder()
+                .fileName(fileName)
+                .open();
+        map = store.openMap(mapName);
+    }
+
+    public void put(String key, Object obj){
+        map.put(key, obj);
+        store.commit();
+    }
+
+    public Object get(String key){
+        return map.get(key);
+    }
+
+    public void close(){
+        store.close();
+    }
+}
