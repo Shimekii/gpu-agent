99701bf8e350f9dfe57b44c0c43b970fd2caa329
Nikita <itname@bk.ru>
2026-03-25 20:22:39 +0700

    Добавлен метод возвращающий timestamp в виде строки для использования по ключу

--- src/main/java/org/example/domain/GPUMetric.java
+++ src/main/java/org/example/domain/GPUMetric.java
@@ -1,7 +1,11 @@
 package org.example.domain;
 
+import java.sql.Date;
 import java.time.Instant;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class GPUMetric {
     private final Instant timestamp;
@@ -12,6 +16,12 @@ public class GPUMetric {
         this.timestamp = timestamp;
         this.freeMemory = freeMemory;
         this.temperature = temperature;
+    }
+
+    public String key(){
+        LocalDateTime ldt = LocalDateTime.ofInstant(timestamp, ZoneId.systemDefault());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        return ldt.format(formatter);
     }
 
     @Override
